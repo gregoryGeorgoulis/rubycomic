19b9681653ed94dc089248bcbd1466179e30d38c
Gregory <gdg201989@gmail.com>
2016-08-18 21:18:46 -0400

    require date

--- app/controllers/comics_controller.rb
+++ app/controllers/comics_controller.rb
@@ -1,3 +1,4 @@
+require 'date'
 class ComicsController < ApplicationController
 	before_action :set_comic, only: [:show, :edit, :update, :destroy]
 
@@ -7,7 +8,7 @@ class ComicsController < ApplicationController
     end
 
   def show
-  	ts = Date.now
+  	ts = Time.now
   	pub_key = Rails.application.secrets.PUBLIC_KEY
   	pri_key = Rails.application.secrets.PRIVATE_KEY
   	hash = Digest::MD5.hexdigest( ts.to_s + pri_key + pub_key )
